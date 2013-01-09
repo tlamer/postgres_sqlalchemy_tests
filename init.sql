b6f56d33379a5439dab261dd298d6d4c65bde9c6
tlamer <tlamer@gmail.com>
2013-01-09 09:22:09 +0100

    sources

---
+++ init.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS dim_customers;
+DROP TABLE IF EXISTS src_customers;
+DROP TABLE IF EXISTS tmp_new;
+
+CREATE TABLE src_customers (
+    id integer PRIMARY KEY,
+    name varchar(128),
+    address varchar(256)
+);
+
+CREATE TABLE dim_customers (
+    id integer,
+    name varchar(128),
+    address varchar(256),
+    surrogate SERIAL PRIMARY KEY
+);
+
+INSERT INTO src_customers (id, name, address) VALUES (1,'peter','kapicova');
+INSERT INTO src_customers (id, name, address) VALUES (2,'danko','romanova');
+INSERT INTO src_customers (id, name, address) VALUES (3,'stivi','hornadolna');
+
---
+++ scd.sql
@@ -0,0 +1,14 @@
+﻿-- new database entries
+INSERT INTO dim_customers
+    SELECT s.* FROM (SELECT id FROM src_customers EXCEPT SELECT id FROM dim_customers) n
+    JOIN src_customers s ON n.id=s.id;
+
+-- changed entries
+INSERT INTO dim_customers
+    (SELECT s.* FROM
+        (SELECT * FROM dim_customers WHERE surrogate=ANY
+            -- check every id with largest surrogate number (against the last change of entry)
+            (SELECT max(surrogate) FROM dim_customers GROUP BY id)) d
+        JOIN src_customers s ON s.id=d.id
+        WHERE (d.name!=s.name OR d.address!=s.address));
+
